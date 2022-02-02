bec24f055de07027accb973d6a4ca6c6082ba295
Alexandru Chiritescu <alex@alexchiri.com>
2022-02-02 11:12:24 +0100

    Provide scheduler config to k3s install

--- create-cluster.ps1
+++ create-cluster.ps1
@@ -9,7 +9,7 @@ $PUBLIC_KEY = [IO.File]::ReadAllText(".\id_rsa.pub")
 (Get-Content -path .\node-cloud-config.yaml -Raw) -replace '{PUBLIC_KEY}', $PUBLIC_KEY | multipass launch --name node2 --cloud-init -
 
 $MASTER_IP=(multipass info master --format json | jq -r .info.master.ipv4[0])
-multipass exec control -- k3sup install --ip $MASTER_IP --user ubuntu --local-path /home/ubuntu/.kube/config --k3s-extra-args '--no-deploy traefik'
+multipass exec control -- k3sup install --ip $MASTER_IP --user ubuntu --local-path /home/ubuntu/.kube/config --k3s-extra-args "--no-deploy traefik --kube-scheduler-arg 'config=/etc/sched_config.yaml'" --k3s-version 'v1.22.5+k3s1'
 $NODE1_IP=(multipass info node1 --format json | jq -r .info.node1.ipv4[0])
 multipass exec control -- k3sup join --ip $NODE1_IP --server-ip $MASTER_IP --user ubuntu
 $NODE2_IP=(multipass info node2 --format json | jq -r .info.node2.ipv4[0])
--- create-cluster.sh
+++ create-cluster.sh
@@ -5,12 +5,12 @@ echo "Control IP: $(multipass info control --format json | jq -r .info.control.i
 
 multipass transfer control:/home/ubuntu/.ssh/id_rsa.pub ./id_rsa.pub
 PUBLIC_KEY="$(cat ./id_rsa.pub)"
-sed "s#{PUBLIC_KEY}#$PUBLIC_KEY#" node-cloud-config.yaml | multipass launch --name master --mem 3G --cpus 2 --cloud-init -
+sed "s#{PUBLIC_KEY}#$PUBLIC_KEY#" master-cloud-config.yaml | multipass launch --name master --mem 3G --cpus 2 --cloud-init -
 sed "s#{PUBLIC_KEY}#$PUBLIC_KEY#" node-cloud-config.yaml | multipass launch --name node1 --cloud-init -
 sed "s#{PUBLIC_KEY}#$PUBLIC_KEY#" node-cloud-config.yaml | multipass launch --name node2 --cloud-init -
 
 MASTER_IP="$(multipass info master --format json | jq -r .info.master.ipv4[0])"
-multipass exec control -- k3sup install --ip $MASTER_IP --user ubuntu --local-path /home/ubuntu/.kube/config --k3s-extra-args '--no-deploy traefik' --k3s-version 'v1.22.5+k3s1'
+multipass exec control -- k3sup install --ip $MASTER_IP --user ubuntu --local-path /home/ubuntu/.kube/config --k3s-extra-args "--no-deploy traefik --kube-scheduler-arg 'config=/etc/sched_config.yaml'" --k3s-version 'v1.22.5+k3s1'
 NODE1_IP="$(multipass info node1 --format json | jq -r .info.node1.ipv4[0])"
 multipass exec control -- k3sup join --ip $NODE1_IP --server-ip $MASTER_IP --user ubuntu
 NODE2_IP="$(multipass info node2 --format json | jq -r .info.node2.ipv4[0])"
